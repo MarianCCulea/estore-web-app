30bafc674f9b6068b0f4b3916325b8bfececfc88
Marian-Claudiu Culea <marianclaudiu.culea@gmail.com>
2023-03-28 19:41:12 +0200

    feat: create storybook template and cat card

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { domains: ["links.papareact.com", "fakestoreapi.com"] },
+  images: {
+    domains: ['links.papareact.com', 'fakestoreapi.com', 'i.pravatar.cc'],
+  },
   experimental: { appDir: true },
 };
 
